6c4fb3fda6ac862f906332844363197447c7dbc5
mouleshm <moulesh.manikandan@colorado.edu>
2026-05-11 13:45:32 -0600

    modified the code

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://payops-backend-app-f4h4fwgvazbbgheu.centralus-01.azurewebsites.net";
 
 export const askAgent = async (payload) => {
   const response = await axios.post(`${API_BASE_URL}/agent/ask`, payload);
